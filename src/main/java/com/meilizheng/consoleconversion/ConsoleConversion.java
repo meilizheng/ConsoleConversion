f53077edc19618e5f09d8df04ce4c0419981d2c3
meilizheng <116130097+meilizheng@users.noreply.github.com>
2023-10-22 12:04:24 -0700

    Initial commit

---
+++ src/main/java/com/meilizheng/consoleconversion/ConsoleConversion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.meilizheng.consoleconversion;
+
+/**
+ *
+ * @author belle
+ */
+public class ConsoleConversion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/meilizheng/consoleconversion/Player.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.meilizheng.consoleconversion;
+
+/**
+ *
+ * @author belle
+ */
+public class Player {
+
+//Field
+
+    private String _name;
+    private int _number;   
+
+
+//Constructor
+public Player(String playerName, int playerNumber)
+{   _name = playerName;
+    _number = playerNumber;
+}
+
+//Property
+
+    public String getName() {
+        return _name;
+    }
+
+    public void setName(String _name) {
+        this._name = _name;
+    }
+
+    public int getNumber() {
+        return _number;
+    }
+    
+       public void setNumber(int _number) {
+        this._number = _number;
+    }
+
+        }
